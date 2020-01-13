2b73bc1abe26d5f3dbcc12c8924f5097a4f5ec4a
Tuzki <254140756@qq.com>
2020-01-13 17:37:14 +0800

    add 积分mq

---
+++ ssm/src/main/java/cn/spring/ssm/mq/AddIntegralMq.java
@@ -0,0 +1,39 @@
+package cn.spring.ssm.mq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Package: cn.spring.ssm.mq
+ * User: 25414
+ * Date: 2020/1/13
+ * Time: 16:50
+ * Description:
+ */
+@Component
+@Slf4j
+public class AddIntegralMq {
+    @Autowired
+    @Qualifier("topicJms")
+    private JmsTemplate jmsTemplate;
+
+    @Value("${spring.activemq.topic.integral}")
+    private String pub;
+
+
+    /**
+     * 给下游系统下发数据
+     *
+     * @param data
+     */
+    public void distributeData(String data) throws Exception {
+        log.info("###DataDistributeMq### producer data = {}", data);
+        log.info("###DataDistributeMq### producer deliveryMode = {}, isTransacted = {}, SessionAcknowledge = {}", jmsTemplate.getDeliveryMode(), jmsTemplate.isSessionTransacted(), jmsTemplate.getSessionAcknowledgeMode());
+        jmsTemplate.convertAndSend(pub, data);
+    }
+}
